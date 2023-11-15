73eac1adfffa8236c024e8c7bfa26d7bcf83afad
Ben Jones <benalexanderjones@gmail.com>
2023-11-15 15:19:12 +0000

    update

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,45 @@
+class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[ show edit ]
+
+  # GET /reviews or /reviews.json
+  def index
+    @reviews = Review.all
+  end
+
+  # GET /reviews/1 or /reviews/1.json
+  def show
+  end
+
+  # GET /reviews/new
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  # GET /reviews/1/edit
+  def edit
+  end
+
+  # POST /reviews or /reviews.json
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def review_params
+      params.require(:review).permit(:rating, :content)
+    end
+end
